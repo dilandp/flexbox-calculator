1dad00e7693f06b58cc532d955ecf872d1c7cb40
Dilan DP <dilandp16@gmail.com>
2020-10-17 01:24:41 +0700

    Add function to do calculation
    #Issue MINUS function:
     - after calculation we must reset displayNumber to do next calculation

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -78,3 +78,20 @@ function handleOperator(operator) {
 		alert("Operator sudah ditetapkan");
 	}
 }
+
+function performCalculation() {
+	if(calculator.firstNumber == null || calculator.operator == null) {
+		alert("Anda belum menetapkan Operator");
+		return;
+	}
+
+	let result = 0;
+
+	if(calculator.operator === "+") {
+		result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+	}else{
+		result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+	}
+
+	calculator.displayNumber = result;
+}
